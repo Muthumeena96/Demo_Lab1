fd6c8cfe661f03f40bd483e25200410a3c9c96db
Muthumeena96 <muthumeena.a@hcltech.com>
2026-03-01 22:07:58 +0530

    Create deploy.sh

---
+++ Create deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "-----------------------------------"
+echo "   Deployment Started on Runner    "
+echo "-----------------------------------"
+
+echo "Files received:"
+ls -la deploy-artifacts/
+
+echo "Running Deployment Steps..."
+echo "Deployment Completed Successfully!"
